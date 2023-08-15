41ee34ee8bca1894413a4b1855404e13acba9cfe
Alejandro Romero Rodríguez <88353924+Aleromero77@users.noreply.github.com>
2023-08-15 14:08:56 -0600

    Yol1.0 (#3) (#4)
    * Interfaz login
    
    * Login en FrontEnd
    
    * login frontend
    
    * Correccion de la funcion.jsx
    
    * funciones show_alert y status
    
    * Cambios en los seeders
    
    ---------
    
    Co-authored-by: Carlos Zapata <carloszapatalor@outlook.com>
    Co-authored-by: Carlos Zapata <carloszapatalor@outlook.comm>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,46 @@
 //import logo from './logo.svg';
 import './App.css';
-import React from 'react';
+import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Nav from './components/Nav';
 import  Home  from "./components/Home/Home";
-
-//Componentes Terapeutas
-import MostrarTerapeutas from './components/Terapeutas/MostrarTerapeutas';
+import CrearDepartamentos from "./components/Views/Departamentos/Create";
+import EditarDepartamentos from "./components/Views/Departamentos/Edit";
+import Departamentos from "./components/Views/Departamentos/index";
+import Empleados from "./components/Views/Empleados/Index";
+import Login from "./components/Views/Login";
+import Register from "./components/Views/Register";
+import ProtecteRoutes from "./components/ProtecteRoutes"
 import EditarTerapeuta from './components/Terapeutas/EditarTerapeuta';
 import CrearTerapeuta from './components/Terapeutas/CrearTerapeuta';
-
-//Componentes Pacientes
+import MostrarTerapeutas from './components/Terapeutas/MostrarTerapeutas';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 function App() {
   return (
     <div className="App">
       
       <BrowserRouter>
+      <Nav />
+      <Routes>
+      <Route path='/' element={<Home/>} />
+      </Routes>
+      
         <Routes>
-          <Route path='/' element={<Home/>} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+           
+        <Route element={<ProtecteRoutes/>}>
+          <Route path='/departamentos' element={<Departamentos />} />
+          <Route path='/crear' element={<CrearDepartamentos />} />
+          <Route path='/edit/:id' element={<EditarDepartamentos />} />
+          <Route path='/empleados' element={<Empleados />} />
+
           <Route path='/terapeuta' element={<MostrarTerapeutas/>} />
           <Route path='/terapeuta/crear' element={<CrearTerapeuta/>} />
           <Route path='/terapeuta/editar/:id' element={<EditarTerapeuta/>} />
+        </Route>
           
-
         </Routes>
       </BrowserRouter>
      
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-
+import App from './App.js';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.js';
+import axios from 'axios';
+
+window.axios = axios
+
+window.axios.defaults.baseURL = 'http://localhost:8000/'
+window.axios.defaults.headers.common['Accept'] = 'application/json'
+window.axios.defaults.headers.common['Content-Type'] = 'application/json'
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'
+window.axios.defaults.withCredentials = true
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
